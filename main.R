9b0f5135b512842f8c473359b1313a44bbbf2eee
SAMRIT PRAMANIK <samrit.2442@gmail.com>
2024-08-15 17:14:16 +0530

    Project completion and report generated

--- main.R
+++ main.R
@@ -2,17 +2,17 @@
 library(pacman)
 pacman::p_load(tidyverse, plyr, reader, scales)
 
-# unzip("./club_cricket/apl_male_csv2.zip", exdir = paste0("./APL"))
-# unzip("./club_cricket/bbl_male_csv2.zip", exdir = paste0("./BBL"))
-# unzip("./club_cricket/bpl_male_csv2.zip", exdir = paste0("./BPL"))
-# unzip("./club_cricket/cpl_male_csv2.zip", exdir = paste0("./CPL"))
-# unzip("./club_cricket/ctc_male_csv2.zip", exdir = paste0("./CTC"))
-# unzip("./club_cricket/ipl_male_csv2.zip", exdir = paste0("./IPL"))
-# unzip("./club_cricket/lpl_male_csv2.zip", exdir = paste0("./LPL"))
-# unzip("./club_cricket/mlc_male_csv2.zip", exdir = paste0("./MLC"))
-# unzip("./club_cricket/ntb_male_csv2.zip", exdir = paste0("./NTB"))
-# unzip("./club_cricket/psl_male_csv2.zip", exdir = paste0("./PSL"))
-# unzip("./club_cricket/sat_male_csv2.zip", exdir = paste0("./SAT"))
+unzip("./club_cricket/apl_male_csv2.zip", exdir = paste0("./APL"))
+unzip("./club_cricket/bbl_male_csv2.zip", exdir = paste0("./BBL"))
+unzip("./club_cricket/bpl_male_csv2.zip", exdir = paste0("./BPL"))
+unzip("./club_cricket/cpl_male_csv2.zip", exdir = paste0("./CPL"))
+unzip("./club_cricket/ctc_male_csv2.zip", exdir = paste0("./CTC"))
+unzip("./club_cricket/ipl_male_csv2.zip", exdir = paste0("./IPL"))
+unzip("./club_cricket/lpl_male_csv2.zip", exdir = paste0("./LPL"))
+unzip("./club_cricket/mlc_male_csv2.zip", exdir = paste0("./MLC"))
+unzip("./club_cricket/ntb_male_csv2.zip", exdir = paste0("./NTB"))
+unzip("./club_cricket/psl_male_csv2.zip", exdir = paste0("./PSL"))
+unzip("./club_cricket/sat_male_csv2.zip", exdir = paste0("./SAT"))
 
 apl <- read.csv("./APL/all_matches.csv")
 bbl <- read.csv("./BBL/all_matches.csv")
@@ -25,18 +25,6 @@ mlc <- read.csv("./MLC/all_matches.csv")
 ntb <- read.csv("./NTB/all_matches.csv")
 psl <- read.csv("./PSL/all_matches.csv")
 sat <- read.csv("./SAT/all_matches.csv")
-
-# common_plyr <- inner_join(distinct(apl, striker),
-#                           distinct(bbl, striker)) |> 
-#   inner_join(distinct(bpl, striker)) |> 
-#   inner_join(distinct(cpl, striker)) |> 
-#   inner_join(distinct(ctc, striker)) |> 
-#   inner_join(distinct(ipl, striker)) |> 
-#   inner_join(distinct(lpl, striker)) |> 
-#   inner_join(distinct(mlc, striker)) |> 
-#   inner_join(distinct(ntb, striker)) |> 
-#   inner_join(distinct(psl, striker)) |> 
-#   inner_join(distinct(sat, striker))
 
 # Creating Summary stats of top 15 batters and bowlers of each league ----------
 
@@ -68,18 +56,13 @@ master_data <- rbind(apl, bbl, bpl, cpl, ctc, ipl, mlc, ntb, psl, sat)
 
 m_data <- data_clean(master_data)
 
-# m_data$batting_team |> unique() |> sort()
+m_data$batting_team |> unique() |> sort()
 
 # saveRDS(m_data, "m_data.rds")
 
 ################################################################################
-# Run from HERE ----------------------------------------------------------------
-################################################################################
 
-library(pacman)
-pacman::p_load(tidyverse, plyr, reader, scales)
-
-m_data <- readRDS("m_data.rds")
+# m_data <- readRDS("m_data.rds")
   
 batter_stat <- function(cleaned_data) {
   df <- cleaned_data |> 
@@ -118,6 +101,7 @@ bowler_stat <- function(cleaned_data) {
 m_bat <- batter_stat(m_data)
 m_bowl <- bowler_stat(m_data)
 
+# Batter's Table --------------------------------------------------
 
 m_bat_ranked <- m_bat |> 
   dplyr::group_by(year, batting_team) |> 
@@ -138,9 +122,26 @@ m_bat_ranked2 <- m_bat_ranked |>
   dplyr::group_by(striker, year) |> 
   dplyr::slice(1)
 
+# Bowler's Table --------------------------------------------------
 
+m_bowl_ranked <- m_bowl |> 
+  dplyr::group_by(year, bowling_team) |> 
+  dplyr::mutate(R = rank(-wkts_per_ing),
+                S = rank(econ),
+                `T` = (R + S)/2,
+                comb_rank = floor(rank(`T`)),
+                norm_score = 1 - rescale(comb_rank),
+                N = length(R)) |> 
+  dplyr::ungroup() |> 
+  dplyr::group_by(bowler, year) |> 
+  dplyr::mutate(No_teams = length(bowling_team)) |> 
+  dplyr::ungroup() |> 
+  dplyr::filter(No_teams > 1)
 
-
+m_bowl_ranked2 <- m_bowl_ranked |> 
+  dplyr::arrange(desc(norm_score), desc(N)) |> 
+  dplyr::group_by(bowler, year) |> 
+  dplyr::slice(1)
 
 
 
